9dcbae6e5d217eb11e3c0cc827e028a573f55d45
Ahmed Hassanien <Ahmed_Hassanien@epam.com>
2015-04-28 15:44:10 +0200

    Finalized the Media Service Create Media Test

--- src/test/java/com/learn/service/media/DefaultMediaServiceCreateMediaTest.java
+++ src/test/java/com/learn/service/media/DefaultMediaServiceCreateMediaTest.java
@@ -2,41 +2,80 @@ package com.learn.service.media;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class DefaultMediaServiceCreateMediaTest {
 
+    private final String dummyMediaName;
+    private final String dummyMediaExtension;
+    private final byte[] dummyMediaBytes;
+    {
+        dummyMediaName = "MediaName";
+        dummyMediaExtension = "JPG";
+        dummyMediaBytes = dummyMediaName.getBytes();
+    }
     private static MediaService mediaService;
 
     @BeforeClass
-    public static void setUp(){
+    public static void setUp() {
         mediaService = spy(new DefaultMediaService());
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void givenNullMediaObjectWhenCreateMediaThenIllegalArgumentExceptionThrown() {
-
+        mediaService.createMedia(null);
+        fail();
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void givenNullMediaBytesWhenCreateMediaThenIllegalArgumentExceptionThrown() {
-
+        mediaService.createMedia(
+                MediaBusinessObject.newBuilder()
+                        .mediaName(dummyMediaName)
+                        .mediaExtension(dummyMediaExtension)
+                        .mediaBytes(null)
+                        .build()
+        );
+        fail();
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void givenNullMediaNameWhenCreateMediaThenIllegalArgumentExceptionThrown() {
-
+        mediaService.createMedia(
+                MediaBusinessObject.newBuilder()
+                        .mediaName(null)
+                        .mediaExtension(dummyMediaExtension)
+                        .mediaBytes(dummyMediaBytes)
+                        .build()
+        );
+        fail();
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void givenNullMediaExtensionWhenCreateMediaThenIllegalArgumentExceptionThrown() {
-
+        mediaService.createMedia(
+                MediaBusinessObject.newBuilder()
+                        .mediaName(dummyMediaName)
+                        .mediaExtension(null)
+                        .mediaBytes(dummyMediaBytes)
+                        .build()
+        );
+        fail();
     }
 
     @Test
     public void givenValidMediaObjectWhenCreateMediaThenMediaIsStored() {
-
+        verify(mediaService, times(1)).createMedia(
+                MediaBusinessObject.newBuilder()
+                        .mediaName(dummyMediaName)
+                        .mediaExtension(dummyMediaExtension)
+                        .mediaBytes(dummyMediaBytes)
+                        .build()
+        );
     }
 }
